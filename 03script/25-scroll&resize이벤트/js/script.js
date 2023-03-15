7361cb3257016eb6687a6a563e98f44e2a6e1c36
Sohteum <thtwk1123@gmail.com>
2023-03-16 00:04:42 +0900

    review

--- 03script/25-scroll&resize이벤트/js/script.js
+++ 03script/25-scroll&resize이벤트/js/script.js
@@ -1,4 +1,4 @@
- $(document).ready(function () {
+$(document).ready(function () {
   fnChange()//시작할때도 한번 세팅해줘야하니 레디이벤트할때 한번 또 넣어줌
   var raf
   function fnChange() {
@@ -8,7 +8,7 @@
     var winW = $(window).width() //윈도우 안에서 너비
 
 
-    /* $(`.banner1`).css({
+    $(`.banner1`).css({
       'top': `${scrTop}px`,
       'left': `${scrleft}px`,
 
@@ -58,15 +58,15 @@
   $(window).scroll(function () {
 
     raf = requestAnimationFrame(fnChange)
-    /*     raf = requestAnimationFrame(fnChange) *///시스템과부하방지 테크닉 
-
+    //     raf = requestAnimationFrame(fnChange) //시스템과부하방지 테크닉 
     // $(`.banner1`).css({ 'top': `${scrTop + winH*0.5}px` }) 
     //fixed처럼보임/여기서 +100을해주면 계속 웹브라우저의 top으로부터 떨어뜨려놓을 수 있음, 나누기보다 곱하기가 더 빠르고 소수점 문제가 적어서 선호함 
   }).resize(function () {
     raf = requestAnimationFrame(fnChange)
   })//스크롤하고난뒤 창사이즈를 바꾸면 가운데에있지않음.그래서 창크기를 바꿔도 그 위치에 있도록 하는 이벤트
-}) 
+})
 
+/* 
 $(document).ready(function () {
   fnChange()//시작할때도 한번 세팅해줘야하니 레디이벤트할때 한번 또 넣어줌
   var raf
@@ -78,23 +78,19 @@ $(document).ready(function () {
     $(`.banner`).each(function () {
       var 넓이곱 = parseFloat($(this).attr(`data-넓이곱`))
       var 높이곱 = parseFloat($(this).attr(`data-높이곱`))
-      $(this).css({'top': `${scrTop + winH * 높이곱}px`, 'left': `${scrleft + winW * 넓이곱}px`})
+      $(this).css({ 'top': `${scrTop + winH * 높이곱}px`, 'left': `${scrleft + winW * 넓이곱}px` })
     })
   }
-//스크립트에서는 픽셀이랑 퍼센트를 더할 수 없음.그러니까 브라우저의 높이의 반절을 구하면 됨. */
+  //스크립트에서는 픽셀이랑 퍼센트를 더할 수 없음.그러니까 브라우저의 높이의 반절을 구하면 됨. 
 
+  $(window).scroll(function () {
+    raf = requestAnimationFrame(fnChange)
+       
+  }).resize(function () {
+    raf = requestAnimationFrame(fnChange)
+  })
 
-$(window).scroll(function () {
-
-  raf = requestAnimationFrame(fnChange)
-  /*     raf = requestAnimationFrame(fnChange) *///시스템과부하방지 테크닉 
-
-  // $(`.banner1`).css({ 'top': `${scrTop + winH*0.5}px` }) 
-  //fixed처럼보임/여기서 +100을해주면 계속 웹브라우저의 top으로부터 떨어뜨려놓을 수 있음, 나누기보다 곱하기가 더 빠르고 소수점 문제가 적어서 선호함 
-}).resize(function () {
-  raf = requestAnimationFrame(fnChange)
-})//스크롤하고난뒤 창사이즈를 바꾸면 가운데에있지않음.그래서 창크기를 바꿔도 그 위치에 있도록 하는 이벤트
-  })//each문
+})//each문 */
 
 
 
--- 05project1/02project/header.php
+++ 05project1/02project/header.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="./css/style.css">
   <script src="https://code.jquery.com/jquery-latest.js"></script>
   <script src="https://kit.fontawesome.com/10bf1730a7.js" crossorigin="anonymous"></script>
-  <script src="./js/script.js"></script>
+  <script src="./js/nav.js"></script>
   <title>Document</title>
 </head>
 
@@ -22,6 +22,7 @@
         <a href="#">회원가입</a>
       </nav>
     </div>
+
     <div class="bottom">
       <h1>
         <a href="./index.php">
@@ -35,7 +36,10 @@
       <nav class="nav_bar sm">
         <?php include "nav-bar.php" ?>
       </nav>
-      <button class="btn"><i class="fa-solid fa-bars"></i></button>
+      <button class="btn">
+        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-xmark close"></i>
+      </button>
     </div>
 
   </header>
