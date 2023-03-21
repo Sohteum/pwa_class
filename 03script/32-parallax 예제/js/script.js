57e228539fe278345b7c3cdbfe724bed4fd28828
Sohteum <thtwk1123@gmail.com>
2023-03-21 18:18:45 +0900

    .

--- 03script/32-parallax 예제/js/script.js
+++ 03script/32-parallax 예제/js/script.js
@@ -1,14 +1,16 @@
 $(function () {
 
-  $(`section`).each(function () {
-    var w = $(this).children('div').innerWidth()
-    var winW = $(this).innerWidth()
+
+
+
 /*     var offL = $(this).offset().left */
-    var offT = $(this).offset().top
-    var meta = 0 - Math.abs(scrT -( offT - winW*0.5)) * 0.1
-    if (meta < 0) { meta = 0 }
-    $(this).css({'transform':`translateX(${meta})`})
-  })
+    var offT = $(`section`).offset().top
+    var meta = 0 + Math.abs(scrT -(offT)) * 0.1
+    if (meta > 50) { meta = 50 }
+    $(`section`).css({'width': `${meta}`})
+
 
 
 })
+
+/* 초기값 + 절대값(스크롤위치 - (스크롤목적지)) * 양수 */
