b7272169f48ef623d2e575463a7537fe62785506
Sohteum <thtwk1123@gmail.com>
2023-04-12 08:16:26 +0900

    .

--- 05react/react-app/src/App.js
+++ 05react/react-app/src/App.js
@@ -1,7 +1,7 @@
-/* 이 위쪽은 임폴트하는 용도로 사용이 됨  */
 
 import { useState } from "react";
 //모듈즈안에있는것들은 경로없이 불러올수있음 .js는 생략된것.{}니까 익스폴트한거지. 함수야.
+
 
 function App() { /* 이걸 컴포넌트라고 함 */
   let [_n, _setNumber] = useState(1)
@@ -13,7 +13,7 @@ function App() { /* 이걸 컴포넌트라고 함 */
     //let number = number
     //_setNumber(++n)
 
-    _setNumber(prev => prev + 1 )
+    _setNumber(prev => prev + 1)
     console.log(_n);
 
   }//fnClickHandler
