9ad155d3ba6e5a2d6aa2a0abe652f10c59c06a17
Sohteum <thtwk1123@gmail.com>
2023-04-14 07:24:40 +0900

    .

--- 05react/react-app/src/components/CompModal.jsx
+++ 05react/react-app/src/components/CompModal.jsx
@@ -1,21 +1,13 @@
 import React, { useState } from 'react';
 
-const CompModal = ({_point}) => {
-
-
-
-
+const CompModal = ({ _point }) => {
   return (
-    
     <div className='modal'>
-    <p>
-      {_point}점입니다 <br/>
-      <button>다시시작</button>
-    </p>
-  </div>
-
-
-
+      <p>
+        {_point}점입니다 <br />
+        <button>다시시작</button>
+      </p>
+    </div>
   );
 };
 
