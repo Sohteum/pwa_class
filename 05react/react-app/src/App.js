00aa88ec78c90d500ee70867d8514698612af61a
Sohteum <thtwk1123@gmail.com>
2023-04-13 18:19:35 +0900

    .

--- 05react/react-app/src/App.js
+++ 05react/react-app/src/App.js
@@ -8,25 +8,36 @@ function App() {
   const [_point, _setPoint] = useState(0)
   const [_showModal, _setShowModal] = useState(false)
 
-const fnModalHandler = function(){
-  _setShowModal(true)
-}
+  const fnModalHandler = function () {
+    let point = 0
+    let inputs = document.querySelectorAll('form')
+    inputs.forEach(v => {
+      let userNum = parseInt(v.querySelector('.userNum').value)
+      let num1 = parseInt(v.querySelector('.num1').innerText)
+      let num2 = parseInt(v.querySelector('.num2').innerText)
+      if (num1 + num2 === userNum) _setPoint(prev => prev + 1)
+
+    })
+    console.log(point);
+    //    _setPoint(point)
+    _setShowModal(true)
+  }
 
   return (
     <>
       <h1>더하기 계산</h1>
       <hr />
-      <Sum _setPoint={_setPoint} />{/* 내가 이걸 물려주는데 나는 셋포인트라고 부르지만 너는 a라고 불러 너는 a라는 박스에 담아서 써 하지만 귀찮으니까 그냥 같은 이름으로 쓴다.*/}
+      <Sum _setPoint={_setPoint} _point={_point} />{/* 내가 이걸 물려주는데 나는 셋포인트라고 부르지만 너는 a라고 불러 너는 a라는 박스에 담아서 써 하지만 귀찮으니까 그냥 같은 이름으로 쓴다.*/}
       <hr />
-      <Sum _setPoint={_setPoint} />
+      <Sum _setPoint={_setPoint} _point={_point} />
       <hr />
-      <Sum _setPoint={_setPoint} />
+      <Sum _setPoint={_setPoint} _point={_point} />
       <hr />
-      <Sum _setPoint={_setPoint} />
+      <Sum _setPoint={_setPoint} _point={_point} />
       <hr />
-      <Sum _setPoint={_setPoint} />
+      <Sum _setPoint={_setPoint} _point={_point} />
       <hr />
-      
+
       <button onClick={fnModalHandler}>점수확인</button>
       {(_showModal === true) && <CompModal _point={_point} />}
 
--- 05react/react-app/src/components/Sum.jsx
+++ 05react/react-app/src/components/Sum.jsx
@@ -5,29 +5,31 @@ const fnGetNum = () => Math.floor(Math.random() * 10)
 
 
 
-const Sum = () => {
+const Sum = ({ _setPoint, _point }) => {
 
   const [_num1, _setNum1] = useState(fnGetNum)
   const [_num2, _setNum2] = useState(fnGetNum)
-  
-  const refInput = useRef()
 
+  const refInput = useRef()
 
 
   let fnSubmitHandler = function (e) {
     e.preventDefault()
     // let userNum = parseInt(refInput.current.value)
     // if (_num1 + _num2 === userNum) _setPoint(prev=> prev+1)
+  }
 
+  let fnInputHandler = function (e) { //해당이벤트가 이루어지고있는애
+    /*     let userNum = parseInt(e.target.value) */
   }
 
   return (
     <form onSubmit={fnSubmitHandler}>
-      <span>{_num1}</span>
+      <span className='num1'>{_num1}</span>
       +
-      <span>{_num2}</span>
+      <span className='num2'>{_num2}</span>
       =
-      <input ref={refInput} type="number" min="0" max="20" required />
+      <input className='userNum' onInput={fnInputHandler} ref={refInput} type="number" min="0" max="20" required />
     </form>
   );
 };
