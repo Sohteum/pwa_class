bcb6ef36629def83f55c42250277e4d8bfa1e5ae
Sohteum <thtwk1123@gmail.com>
2023-04-16 13:22:20 +0900

    .

--- 04EcmaScript6/05-event forEach/js/script.js
+++ 04EcmaScript6/05-event forEach/js/script.js
@@ -1,14 +1,14 @@
 
 {
-  document.querySelectorAll(`.ex1 button`).forEach((btn) => {
-    btn.addEventListener(`click`, e => {
-
+  document.querySelectorAll(`.ex1 button`).forEach((v) => {
+    v.addEventListener(`click`, e => {
+      //이벤트리스너는 하나의 객체에만 가능
       console.log(e.target.innerText);
     })
   })
 }
 //제이쿼리이치는 이벤트인데 여기서 포이치는 그냥 반복문일 뿐. 
-//btn는 차례에 해당하는 그 애를 말함 한개를 네번반복한거 포이치가 네번하는거라는거지 btn 자체는 하나임 하나한테는 이벤트추가가 가능
+//v는 차례에 해당하는 그 애를 말함 한개를 네번반복한거 포이치가 네번하는거라는거지 btn 자체는 하나임 하나한테는 이벤트추가가 가능
 
 //클릭한애가 이쩜타겟. 그래서 걔한테서 안에 글자를 받아옴. 
 {
@@ -21,7 +21,7 @@
   })
 }
 
-//값을 세번 가져오는것보다 증감연산을 사용하는것이 편함. 초기화를 해야하는데 초기화를 버튼을 누를때마다 하니까 안에서 0을 만든다.
+//값을 세번 가져오는것보다 증감연산을 사용하는것이 편함. 초기화를 해야하는데 초기화를 버튼을 누를때마다 하니까 안에서 0을 만든다. 다시말해 클릭할때마다 0을 만들고싶으면 안쪽에서 선언, 클릭과 상관없이 값이 쌓이는거면 밖에서 만들면 됨
 
 {
   let inputs = document.querySelectorAll(`.ex3 input`)
@@ -38,13 +38,13 @@
 }
 
 {
- const fnBox = (exNum) => {
+  const fnBox = (exNum) => {
     document.querySelectorAll(`.ex${exNum} .box`).forEach(n => {
       n.style.background = n.innerText
     })
   }
   document.querySelectorAll(`.ex4 button,.ex5 button`).forEach(button => {
-    button.addEventListener(`click`, e=> {
+    button.addEventListener(`click`, e => {
       fnBox(e.target.value)
     })
   })
