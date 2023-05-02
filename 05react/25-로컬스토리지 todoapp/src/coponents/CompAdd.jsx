4fff28572256ae24d00efa1ecc798af3b113bf73
Sohteum <thtwk1123@gmail.com>
2023-05-03 07:54:35 +0900

    .

--- 05react/25-로컬스토리지 todoapp/src/coponents/CompAdd.jsx
+++ 05react/25-로컬스토리지 todoapp/src/coponents/CompAdd.jsx
@@ -3,34 +3,52 @@ import { AppContext } from '../App';
 import { useNavigate } from 'react-router-dom';
 
 const CompAdd = () => {
-
-  const { _orgArr, _setOrgArr, _outputArr, _setOutputArr } = useContext(AppContext)
-  const nav = useNavigate()
+  const { _orgArr, _setOrgArr, _outputArr, _setOutputArr } = useContext(AppContext);
+  const nav = useNavigate();
 
   const fnSubmithandler = function (e) {
-    e.preventDefault()
-    const id = Date.now()
-    const title = document.querySelector('input[type=text]').value
-    const date = document.querySelector('input[type=date]').value
-    const time = document.querySelector('input[type=time]').value
-    const desc = document.querySelector('textarea').value
-    const obj = { id, title, date, time, desc } //아웃풋, 원본배열, 
-    const arr = [..._orgArr, obj]//
-    _setOrgArr(arr)
-    _setOutputArr(arr)
-    window.localStorage.setItem('todoStorage', JSON.stringify(arr))//모두가 쓰는 파일형태인 제이슨 형태로만 써야함.
+    e.preventDefault();
+    const id = Date.now();
+    const title = document.querySelector('input[type=text]').value;
+    const date = document.querySelector('input[type=date]').value;
+    const time = document.querySelector('input[type=time]').value;
+    const desc = document.querySelector('textarea').value;
+    const obj = { id, title, date, time, desc }; //여기서 리스트한데 아이디를 넘겨줬다고 할 수 있는건가?????????????
+    const arr = [..._orgArr, obj]; //
+    _setOrgArr(arr);
+    _setOutputArr(arr);
+    window.localStorage.setItem('todoStorage', JSON.stringify(arr)); //모두가 쓰는 파일형태인 제이슨 형태로만 써야함.
     //location.href//이건 새로고침이 일어남
-    nav("/")
-  }
-
+    nav('/');
+  };
 
   return (
     <form onSubmit={fnSubmithandler}>
-      <p>일정명 : <input type="text" required /></p>
-      <p>날짜 : <input type="date" required /></p>
-      <p>시간 : <input type="time" required /></p>
-      <p>세부내용 : <textarea required></textarea></p>
-      <button >일정추가하기</button>
+      <p>
+        일정명 :{' '}
+        <input
+          type='text'
+          required
+        />
+      </p>
+      <p>
+        날짜 :{' '}
+        <input
+          type='date'
+          required
+        />
+      </p>
+      <p>
+        시간 :{' '}
+        <input
+          type='time'
+          required
+        />
+      </p>
+      <p>
+        세부내용 : <textarea required></textarea>
+      </p>
+      <button>일정추가하기</button>
     </form>
   );
 };
