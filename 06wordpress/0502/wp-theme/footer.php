d8b45d9a7d3ae862706a7276a5f216a9bacaaac9
Sohteum <thtwk1123@gmail.com>
2023-05-02 14:33:17 +0900

    .

---
+++ 06wordpress/0502/wp-theme/footer.php
@@ -0,0 +1,7 @@
+<footer>
+  footer
+</footer>
+<!--site-footer -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ 06wordpress/0502/wp-theme/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+  <title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <header>
+    header
+  </header>
